79b94e47968149f6dc7ac9cc02d73714a6986ef7
Pimenta <a7311@ualg.pt>
2025-01-20 01:05:00 +0000

    Adiciona imports e configuração básica de diretórios e variáveis globais.

---
+++ External.py
@@ -0,0 +1,16 @@
+import cv2
+import mediapipe as mp
+import socket
+import time
+import os
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_FILENAME = "efficientdet_lite0.tflite"
+MODEL_PATH = "C:/Users/admin/Desktop/Visu/efficientdet_lite0.tflite"
+
+if os.path.exists(MODEL_PATH):
+    pass
+else:
+    pass
